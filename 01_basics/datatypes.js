ce3df58fdb6e4684f68d15c7095d5a3ee0dbcd72
codeStruck101 <144527676+codeStruck101@users.noreply.github.com>
2025-06-26 13:54:57 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) since we're using node js 
+
+console.log(3+3)
+console.log("anjana"); //code readability should be high
+
+let name = "Hitesh"
+let age = 18
+let isLoggedIn = false
+
+//number => 2^53
+//bigInt => for big number
+//string => ""
+// boolean => true/false
+//null => standalone value
+//undefined => not defined value
+//symbol => unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
+
+
+
+
