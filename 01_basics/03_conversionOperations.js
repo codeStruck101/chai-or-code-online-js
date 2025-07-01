048b310693c7e5a09b67bea3aa03aa8920f5708e
codeStruck101 <144527676+codeStruck101@users.noreply.github.com>
2025-07-01 06:01:05 +0000

    conversion Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -18,3 +18,27 @@ console.log(booleanIsLoggedIn);
 //"" => false
 //" " => true
 //"hitesh" => true
+
+let str1 = "anjana";
+let str2 = " neelayath";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+
+let str = "hello";
+let str4 = "hitesh";
+console.log(str + " " + str4);
+
+console.log(1 + 2 + "3");
+
+console.log(+"");
+
+let num1,num2,num3;
+
+num1 = num2 = num3 = 2+2;
+let num4 = ++num1;
+console.log(num4);
+//(num1++);
+
