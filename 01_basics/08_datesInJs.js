96edc0fdeb603e23aeb820a4b98d17118bb2aa3d
codeStruck101 <144527676+codeStruck101@users.noreply.github.com>
2025-07-02 14:28:52 +0000

    dateAndTime

---
+++ 01_basics/08_datesInJs.js
@@ -0,0 +1,10 @@
+let myDate = new Date()
+//console.log(myDate.toString());
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date(2023,0,23);
+// console.log(myCreatedDate.toDateString());
+//toLocaleString();
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
