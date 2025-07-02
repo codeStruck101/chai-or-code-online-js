dec5c650c9e9be2f3d973f9d7eb53f394ac454a3
codeStruck101 <144527676+codeStruck101@users.noreply.github.com>
2025-07-02 12:54:52 +0000

    math and numbers

---
+++ 01_basics/07_nums_and_math.js
@@ -0,0 +1,27 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+///////////Maths------------------->
+
+console.log(Math);
+console.log(Math.abs(4));
+console.log(Math.round(4.4));
+//Math.floor() // lowest value round off
+//Math.ceil() // highest values round off
+
+console.log((Math.random()*10) + 1 );
+
+const min = 10
+const max = 20
+
+console.log(Math.random() * (max - min + 1))
+
