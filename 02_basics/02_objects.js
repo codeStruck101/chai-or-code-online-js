c34386fdb5a3ae902853799c58eb3716766c7eec
codeStruck101 <144527676+codeStruck101@users.noreply.github.com>
2025-07-03 11:32:58 +0000

    objects

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -13,7 +13,7 @@ const JsUser = {
 
 JsUser.age = 24;
 //Object.freeze(JsUser)
-console.log(JsUser);
+//console.log(JsUser);
 
 JsUser.greeting = function(){
     console.log("Hello Js User")
@@ -23,12 +23,65 @@ JsUser.greeting2 = function(){
     console.log(`Hello Js User, ${this.name}`) //String Interpolation
 }
 
-console.log(JsUser.greeting());
-console.log(JsUser.greeting2());
+// console.log(JsUser.greeting());
+// console.log(JsUser.greeting2());
 
 ///Singleton
 
 const tinderUser = new Object() //Singleton Object
 const tinderUser1 = {} //non-singleton Object
 
-console.log(tinderUser)
+tinderUser.id = "123abc";
+tinderUser.name = "anjana";
+tinderUser.isLoggedIn = false;
+
+//console.log(tinderUser);
+
+const regularUser = {
+    email : "anjana.neelayath@gmail.com",
+    fullname :{
+        userfullname:{
+            firstname : "hitesh",
+            lastname : "choudhary"
+        }
+        
+    }
+}
+
+console.log(regularUser.fullname.userfullname.firstname);
+
+
+//Combining Objects
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+
+const obj3 = Object.assign({},obj1,obj2); //less used
+
+const obj4 = {...obj1,...obj2}
+console.log(obj3);
+console.log(obj4);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); //true
+
+const course ={
+    coursename : "js in hindi",
+    price : "999",
+    courseInstructor : "hitesh"
+}
+
+// object de-structuring
+
+const {price : cost} = course;
+console.log(cost);
+
+//JSON
+
+// {
+//     name : "hitesh",
+    
+// }
