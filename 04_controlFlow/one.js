b6615d98f5a05b88870b717a2b670c59bfec5335
codeStruck101 <144527676+codeStruck101@users.noreply.github.com>
2025-07-10 13:27:55 +0000

    controlflow and iterations

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,25 @@
+if(2 === "2")
+{
+    console.log("these two are equal")
+}
+else
+{
+    console.log("these two are not equal");
+    
+}
+
+//<,>,<=,>=,==,!=,===,!==
+
+const balance = 1000;
+//if(balance > 500) console.log("test"),console.log("test2"); this will work but bad coding practise
+
+
+const userLoggedIn = true;
+const debitCard = true;
+
+ if(userLoggedIn && debitCard)
+{
+    console.log("Allow to buy courses");
+    
+}
+
---
+++ 05_iterations/one.js
@@ -0,0 +1,13 @@
+//for
+for(let i = 0; i< 10;i++)
+{
+    //console.log(`hello for ${i}th time`);
+    
+}
+
+let myArray = ["flash","batman","superman"];
+//console.log(myArray.length);
+
+//break and continue
+
+
