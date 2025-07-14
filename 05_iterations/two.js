262411b4f75e5ea9d8734177b40b66a6e9010f76
codeStruck101 <144527676+codeStruck101@users.noreply.github.com>
2025-07-14 07:27:48 +0000

    maps

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -1,6 +1,15 @@
 let index = 0;
 while(index <= 10)
 {
-    console.log(`Value of index is ${index}`);
+   // console.log(`Value of index is ${index}`);
     
 }
+
+ //Maps
+
+ const map = new Map();
+ map.set('IN',"India");
+ map.set('USA',"United States of America");
+
+ console.log(map);
+ 
